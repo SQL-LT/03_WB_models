689d981bee0f48f9fdab4dd52df234ee49945f58
LoWalker <111641484+LoWalker@users.noreply.github.com>
2023-02-20 12:59:26 +0100

    Drop Table created, Dup Corrected

--- 01_FirstProject/sql/02_users.sql
+++ 01_FirstProject/sql/02_users.sql
@@ -1,3 +1,6 @@
+-- Tabelle
+
+DROP TABLE IF EXISTS  `mydb`.`users`;
 CREATE TABLE IF NOT EXISTS `mydb`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
@@ -10,6 +13,7 @@ DESCRIBE mydb.users;
 
 INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "max", DEFAULT);
 INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "maxine", DEFAULT);
-INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "max", DEFAULT);
+INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "maxl", DEFAULT);
 
+-- Inhalte
 SELECT * FROM mydb.users;
