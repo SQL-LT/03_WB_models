f8d762612a28c2b0833f980dead4a0d7430201fa
LoWalker <111641484+LoWalker@users.noreply.github.com>
2023-02-20 12:27:26 +0100

    Create 02_users.sql
    02_users

---
+++ 01_FirstProject/sql/02_users.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `mydb`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `create_time` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+ENGINE = InnoDB;
+
+DESCRIBE mydb.users;
+
+INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "max", DEFAULT);
+INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "maxine", DEFAULT);
+INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "max", DEFAULT);
+
+SELECT * FROM mydb.users;
